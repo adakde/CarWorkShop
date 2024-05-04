755d2dfab6f257fd6a7e8e3d481e726e1303b660
adakde <kuba-kasprzak12@wp.pl>
2024-05-04 18:56:47 +0200

    Dodaj pliki projektów.

---
+++ Projekt/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+
+namespace Projekt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+    }
+}
