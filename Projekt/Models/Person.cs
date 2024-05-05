40a838367f0ffccb4550fb44fbbe399927d33036
adakde <kuba-kasprzak12@wp.pl>
2024-05-05 18:01:25 +0200

    Update

--- Projekt/Models/Person.cs
+++ Projekt/Models/Person.cs
@@ -2,7 +2,7 @@
 {
     public class Person
     {
-        public string? FirstName { get; set; }
+        public string? FistName { get; set; }
         public string? LastName { get; set; }
     }
 }
