56bb927965f8bad5b8964572b1366f3a43b31c4a
adakde <kuba-kasprzak12@wp.pl>
2024-05-05 19:10:59 +0200

    Clean Code

---
+++ Projekt.Domain/Entities/CarWorkShop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt.Domain.Entities
+{
+    public class CarWorkShop
+    {
+        public required int ID { get; set; }
+        public string Name { get; set; } = default!;
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public CarWorkShopContanct ContactDetails { get; set; } = default!;
+    }
+}
